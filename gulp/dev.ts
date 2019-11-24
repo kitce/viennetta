2d4ed16a35389ad0b1c8052c72a24ea061534849
kitce <kitce.dev@gmail.com>
2019-11-24 19:50:03 +0800

    Refactored Gulp task "dev"

--- gulp/dev.ts
+++ gulp/dev.ts
@@ -1,15 +1,13 @@
 import gulp from 'gulp';
 import typeScss from './type:scss';
 import watchScss from './watch:scss';
-import server from './dev:server';
-
-const watcher = watchScss(typeScss);
+import devServer from './dev:server';
 
 const task = gulp.series(
   typeScss,
   gulp.parallel(
-    watcher,
-    server
+    watchScss(typeScss),
+    devServer
   )
 );
 
