0d42502af9136b318b8d447d023ada50730804f7
kitce <kitce.dev@gmail.com>
2019-11-26 13:53:05 +0800

    Added type-checking to watch:scss

--- gulp/watch:scss.ts
+++ gulp/watch:scss.ts
@@ -1,7 +1,9 @@
 import gulp from 'gulp';
 import { scss } from './files';
 
-const watch = (task: gulp.TaskFunction) => {
+type WatcherFunction = (task: gulp.TaskFunction) => gulp.TaskFunction;
+
+const watch: WatcherFunction = (task) => {
   const _task: gulp.TaskFunction = () => (
     gulp.watch(scss.src, task)
   );
