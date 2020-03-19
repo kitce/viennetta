89ae023479d01ebc06a2c50a003fd83e8c8cc99f
kitce <kitce.dev@gmail.com>
2020-03-19 15:59:03 +0800

    Fixed issue of Webpack HMR always does a full reload

--- gulp/dev:server.ts
+++ gulp/dev:server.ts
@@ -3,16 +3,19 @@ import webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
 import config from '../config/webpack/webpack.config.dev';
 
+const { devServer, stats, watchOptions } = config;
+const options: WebpackDevServer.Configuration = {
+  ...devServer,
+  stats,
+  watchOptions
+};
+
+WebpackDevServer.addDevServerEntrypoints(config, options);
+
 const task: gulp.TaskFunction = () => {
   const compiler = webpack(config);
-  const { devServer, stats, watchOptions } = config;
-  const options: WebpackDevServer.Configuration = {
-    ...devServer,
-    stats,
-    watchOptions
-  };
-  const port = options.port as number;
   const server = new WebpackDevServer(compiler, options);
+  const port = options.port as number;
   server.listen(port);
 };
 
--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,7 @@ import App from './components/App/App.component';
 
 const app = new App('Tiramisu');
 app.say();
+
+if (module.hot) {
+  module.hot.accept();
+}
